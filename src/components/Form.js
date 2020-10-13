fed68b526ca05d0b8c38540fab8a6542e466ec28
djviodes <djviodes26@gmail.com>
2020-10-13 15:31:49 -0700

    Created Form.js and began basic framework for project

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Form = props => {
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: ''
+    })
+
+    return null;
+}
+
+export default Form;
