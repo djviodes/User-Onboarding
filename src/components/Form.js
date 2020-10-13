fd3edbe252557f45e8282a07e1642bfeae0afc46
djviodes <djviodes26@gmail.com>
2020-10-13 15:44:21 -0700

    Added the forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,62 @@
 import React, { useState } from 'react'
 
 const Form = props => {
-    const [form, setForm] = useState({
+    const [user, setUser] = useState({
         name: '',
         email: '',
         password: '',
-        terms: ''
+        terms: false
     })
 
-    return null;
+    const changeHandler = (event) => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return (
+        <form>
+            <label htmlFor='name'>Name:</label>
+            <input 
+                id='name'
+                type='text'
+                name='name'
+                placeholder='Please Enter Your Full Name'
+                value={user.name}
+                onChange={changeHandler}
+            />
+
+            <label htmlFor='email'>Email:</label>
+            <input 
+                id='email'
+                type='email'
+                name='email'
+                placeholder='Please Enter A Valid Email'
+                value={user.email}
+                onChange={changeHandler}
+            />
+
+            <label htmlFor='password'>Password:</label>
+            <input 
+                id='password'
+                type='text'
+                name='password'
+                placeholder='Please Enter Your Password'
+                value={user.password}
+                onChange={changeHandler}
+            />
+
+            <input 
+                id='terms'
+                type='checkbox'
+                name='terms'
+                checked={user.terms}
+                onChange={changeHandler}
+            />
+            <label htmlFor='terms'>Terms & Conditions</label>
+        </form>
+    )
 }
 
 export default Form;
